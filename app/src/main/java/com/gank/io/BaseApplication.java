2e0a1827366798dd81e6ca02e3656c9f56fc9f9a
zouyingjie <yingjiezou1@gmail.com>
2016-09-08 17:09:58 +0800

    添加友盟SDK

---
+++ app/src/main/java/com/gank/io/BaseApplication.java
@@ -0,0 +1,23 @@
+package com.gank.io;
+
+import android.app.Application;
+
+import com.umeng.socialize.PlatformConfig;
+
+/**
+ * Created by zouyingjie on 16/9/8.
+ */
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        //微信 appid appsecret
+        PlatformConfig.setSinaWeibo("3921700954","04b48b094faeb16683c32669824ebdad");
+        //新浪微博 appkey appsecret
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+
+        PlatformConfig.setTwitter("3aIN7fuF685MuZ7jtXkQxalyi", "MK6FEYG63eWcpDFgRYw4w9puJhzDl0tyuqWjZ3M7XJuuG7mMbO");
+    }
+}
--- app/src/main/java/com/gank/io/today/TodayGankActivity.java
+++ app/src/main/java/com/gank/io/today/TodayGankActivity.java
@@ -4,25 +4,31 @@ import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.support.design.widget.BottomSheetDialog;
+import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.gank.io.R;
+import com.gank.io.TestFragment;
 import com.gank.io.girl.GirlActivity;
 import com.gank.io.zhuangbi.ZhuangXActivity;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
 
 public class TodayGankActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -35,9 +41,30 @@ public class TodayGankActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        String[] mPermissionList = new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.CALL_PHONE,Manifest.permission.READ_LOGS,Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.SET_DEBUG_APP,Manifest.permission.SYSTEM_ALERT_WINDOW,Manifest.permission.GET_ACCOUNTS};
+//        ActivityCompat.requestPermissions(TodayGankActivity.this,mPermissionList, 100);
+        final Toolbar toolbar =  (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId){
+                    case R.id.tab_favorites:
+                        initGankViewAndPresenter();
+                        break;
+                    case R.id.tab_friends:
+                        initTestFragment();
+                        break;
+                    case R.id.tab_nearby:
+                        initTestFragment();
+
+                        break;
+                }
+
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
@@ -57,28 +84,66 @@ public class TodayGankActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        initViewAndPresenter();
+        initGankViewAndPresenter();
 
     }
 
-    private void initViewAndPresenter(){
+    private void initGankViewAndPresenter(){
         TodayGankFragment fragment = new TodayGankFragment();
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.fl_today_gank_content, fragment)
+                .replace(R.id.fl_today_gank_content, fragment)
                 .commit();
         new TodayGankPresenter(fragment);
     }
 
+    private void initTestFragment(){
+        TestFragment fragment = new TestFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fl_today_gank_content, fragment)
+                .commit();
+    }
+
     private void showShareDialog() {
-        BottomSheetDialog dialog = new BottomSheetDialog(this);
-        View view = LayoutInflater.from(this).inflate(R.layout.layout_share_bottom_sheet, null);
-        RecyclerView recyclerBottomSheet = (RecyclerView) view.findViewById(R.id.recycler_share);
-        dialog.setContentView(view);
-        dialog.setTitle("分享到");
-        GridLayoutManager manager = new GridLayoutManager(this, 3);
-        recyclerBottomSheet.setLayoutManager(manager);
-        recyclerBottomSheet.setAdapter(new ShareAdpater(this));
-        dialog.show();
+//        BottomSheetDialog dialog = new BottomSheetDialog(this);
+//        View view = LayoutInflater.from(this).inflate(R.layout.layout_share_bottom_sheet, null);
+//        RecyclerView recyclerBottomSheet = (RecyclerView) view.findViewById(R.id.recycler_share);
+//        dialog.setContentView(view);
+//        dialog.setTitle("分享到");
+//        GridLayoutManager manager = new GridLayoutManager(this, 3);
+//        recyclerBottomSheet.setLayoutManager(manager);
+//        recyclerBottomSheet.setAdapter(new ShareAdpater(this));
+//        dialog.show();
+        UMImage image = new UMImage(TodayGankActivity.this, "http://www.umeng.com/images/pic/social/integrated_3.png");
+        UMShareListener listener = new UMShareListener() {
+            @Override
+            public void onResult(SHARE_MEDIA platform) {
+                Toast.makeText(TodayGankActivity.this, platform + " 分享成功啦", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(SHARE_MEDIA platform, Throwable t) {
+                Toast.makeText(TodayGankActivity.this, platform + " 分享失败啦", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel(SHARE_MEDIA platform) {
+                Toast.makeText(TodayGankActivity.this, platform + " 分享取消了", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        final SHARE_MEDIA[] displaylist = new SHARE_MEDIA[]
+                {
+                        SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE,SHARE_MEDIA.SINA,
+                        SHARE_MEDIA.QQ, SHARE_MEDIA.QZONE,SHARE_MEDIA.DOUBAN
+                };
+        new ShareAction(this).setDisplayList( displaylist )
+                .withText( "呵呵" )
+                .withTitle("title")
+                .withTargetUrl("http://www.baidu.com")
+                .withMedia( image )
+                .setListenerList(listener)
+                .open();
+
 
     }
 
